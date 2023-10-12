2fa6a554231b96a4036a641daaaa856467c17f11
fkmoney <fkmoney9@gmail.com>
2023-10-12 15:15:44 -0700

    hub

---
+++ script.js
@@ -0,0 +1,37 @@
+const lTag = document.getElementById("think");
+console.log (lTag)
+
+// lTag.innerHTML= "ISIAQ FARUQ"
+
+lTag.onclick = function() {
+    this.innerHTML= "OLASHILE ISIAQ"
+}
+
+// const hand = document.getElementById("btnss")
+// console.log (hand)
+
+// hand.innerHTML= "SEND"
+
+// hand.ondblclick = function() {
+    // this.innerHTML = "Submit"
+// }
+
+// hand.onclick = function() {
+    this.innerHTML = "GET"
+// }
+
+
+const pTag = document.getElementById("mine");
+console.log (pTag)
+// pTag.textContent= 'allow me to sleep'
+let age =17
+console.log (age);
+
+const myName = ("Adekunle Gold");
+console.log (myName);
+
+pTag.onclick = function() {
+ this.textContent= "Allow me to sleep now";
+   }
+
+ 
